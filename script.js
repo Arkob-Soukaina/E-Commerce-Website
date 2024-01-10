2d6e7edb7eebf13694118f465a441d6cb5e55a00
arkob-soukaina <soukainaark05@gmail.com>
2024-01-10 20:42:47 +0100

    commit

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ window.addEventListener("scroll" , function() {
     header.classList.toggle("sticky" , window.scrollY > 0);
     
 });
+
+
+
+// Show/hide the scroll-to-top button based on the scroll position
+window.onscroll = function () {
+    showScrollToTopButton();
+  };
+
+  function showScrollToTopButton() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+  
+  // Scroll to the top when the button is clicked
+  function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
