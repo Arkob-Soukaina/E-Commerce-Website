52ec8f1baa6ada25f5a823503772cda1d1a53fb9
arkob-soukaina <soukainaark05@gmail.com>
2024-01-11 14:33:14 +0100

    commit

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const header = document.querySelector("header");
 
-window.addEventListener("scroll" , function() {
-    header.classList.toggle("sticky" , window.scrollY > 0);
-    
+
+window.addEventListener("scroll", function() {
+  console.log("Scroll event captured");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
